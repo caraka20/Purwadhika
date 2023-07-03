c2262e0a24a0676852aabb659f52addf7369ac71
caraka20 <carakawijaya2001@gmail.com>
2023-07-03 14:08:08 +0700

    oke

--- Exercise1/003.js
+++ Exercise1/003.js
@@ -1,6 +1,22 @@
 function soal3(param)
 {
     // your code here 
+let pangkat2 = param * param
+let titipIndex = 0
+let titipResult = []
+let result = []
+let nilai = 1
+
+for (let i = 0; i < param; i++) {
+    for (let j = 0; j < param; j++) {
+        titipResult.push(nilai)
+        // titipIndex = nilai
+        nilai++
+    }
+    result.push(titipResult)
+    titipResult =[]
+}
+return result
 }
 
 console.log(soal3(5)) 
@@ -25,4 +41,16 @@ console.log(soal3(4))
         [9,10,11,12],
         [13,14,15,16],
     ]
+*/
+/*
+note : 
+let pangkat2 = param * param
+let titipIndex = 0
+let titipResult = []
+let result = []
+let nilai = 1
+lakukan looping i sebanyak param
+loping j menghasilkan sebanyak param
+titipIndex.push(nilai)
+nilai++
 */
